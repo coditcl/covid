abc4f01bc2492c063860840b0fcf07665725552f
Nicolas <ndonosofs@gmail.com>
2020-06-24 22:49:40 -0400

    got first covid us stats done

---
+++ src/data/parsers.js
@@ -0,0 +1,20 @@
+import format from './format';
+import moment from 'moment';
+function usStats(data){
+   const [usStatsRaw] = data; 
+   
+ return{
+    cases: format.number(usStatsRaw.positive),
+    deaths: format.number(usStatsRaw.death),
+    recovered: format.number(usStatsRaw.recovered),
+    ventilator: format.number(usStatsRaw.onVentilatorCurrently),
+    hospitalized: format.number(usStatsRaw.hospitalized),
+    icu: format.number(usStatsRaw.inIcuCurrently),
+    tested: format.number(usStatsRaw.totalTestResults),
+    updated: moment(usStatsRaw.lastModified).format('LLLL')
+ };
+}
+
+export default{
+    usStats,
+};
